c0c9f09f3523bb3086cbb5b5e67657c91f937347
Nataliya Khomenko <NataliaHomenko83@gmail.com>
2024-05-31 00:12:14 +0300

    redis

--- src/services/auth.py
+++ src/services/auth.py
@@ -1,15 +1,17 @@
-from datetime import datetime, timedelta, timezone
+import pickle
+from datetime import datetime, timedelta
 from typing import Optional
+
 import redis
 from fastapi import Depends, HTTPException, status
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 from jose import JWTError, jwt
-from src.conf.config import config
 
 from src.database.db import get_db
 from src.repository import users as repository_users
+from src.conf.config import config
 
 
 class Auth:
@@ -28,45 +30,64 @@ class Auth:
 
     def get_password_hash(self, password: str):
         return self.pwd_context.hash(password)
-    
 
     oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/auth/login")
-    
 
-    async def create_access_token(self, data: dict, expires_delta: Optional[float] = None):
+    # define a function to generate a new access token
+    async def create_access_token(
+        self, data: dict, expires_delta: Optional[float] = None
+    ):
         to_encode = data.copy()
         if expires_delta:
             expire = datetime.utcnow() + timedelta(seconds=expires_delta)
         else:
             expire = datetime.utcnow() + timedelta(minutes=15)
-        to_encode.update({"iat": datetime.utcnow(), "exp": expire, "scope": "access_token"})
-        encoded_access_token = jwt.encode(to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM)
+        to_encode.update(
+            {"iat": datetime.utcnow(), "exp": expire, "scope": "access_token"}
+        )
+        encoded_access_token = jwt.encode(
+            to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM
+        )
         return encoded_access_token
-    
 
-    async def create_refresh_token(self, data: dict, expires_delta: Optional[float] = None):
+    # define a function to generate a new refresh token
+    async def create_refresh_token(
+        self, data: dict, expires_delta: Optional[float] = None
+    ):
         to_encode = data.copy()
         if expires_delta:
             expire = datetime.utcnow() + timedelta(seconds=expires_delta)
         else:
             expire = datetime.utcnow() + timedelta(days=7)
-        to_encode.update({"iat": datetime.utcnow(), "exp": expire, "scope": "refresh_token"})
-        encoded_refresh_token = jwt.encode(to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM)
+        to_encode.update(
+            {"iat": datetime.utcnow(), "exp": expire, "scope": "refresh_token"}
+        )
+        encoded_refresh_token = jwt.encode(
+            to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM
+        )
         return encoded_refresh_token
-    
 
     async def decode_refresh_token(self, refresh_token: str):
         try:
-            payload = jwt.decode(refresh_token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
-            if payload['scope'] == 'refresh_token':
-                email = payload['sub']
+            payload = jwt.decode(
+                refresh_token, self.SECRET_KEY, algorithms=[self.ALGORITHM]
+            )
+            if payload["scope"] == "refresh_token":
+                email = payload["sub"]
                 return email
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid scope for token')
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid scope for token",
+            )
         except JWTError:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate credentials')
-        
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate credentials",
+            )
 
-    async def get_current_user(self, token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
+    async def get_current_user(
+        self, token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)
+    ):
         credentials_exception = HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Could not validate credentials",
@@ -76,7 +97,7 @@ class Auth:
         try:
             # Decode JWT
             payload = jwt.decode(token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
-            if payload['scope'] == 'access_token':
+            if payload["scope"] == "access_token":
                 email = payload["sub"]
                 if email is None:
                     raise credentials_exception
@@ -85,20 +106,28 @@ class Auth:
         except JWTError as e:
             raise credentials_exception
 
-        user = await repository_users.get_user_by_email(email, db)
-        if user is None:
-            raise credentials_exception
-        return user
+        user_hash = str(email)
 
+        user = self.cache.get(user_hash)
+
+        if user is None:
+            print("User from database")
+            user = await repository_users.get_user_by_email(email, db)
+            if user is None:
+                raise credentials_exception
+            self.cache.set(user_hash, pickle.dumps(user))
+            self.cache.expire(user_hash, 300)
+        else:
+            print("User from cache")
+            user = pickle.loads(user)
+        return user
 
     def create_email_token(self, data: dict):
         to_encode = data.copy()
-        now = datetime.now(timezone.utc)
-        expire = now + timedelta(days=1)
-        to_encode.update({"iat": now, "exp": expire})
+        expire = datetime.utcnow() + timedelta(days=1)
+        to_encode.update({"iat": datetime.utcnow(), "exp": expire})
         token = jwt.encode(to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM)
         return token
-
 
     async def get_email_from_token(self, token: str):
         try:
@@ -113,9 +142,4 @@ class Auth:
             )
 
 
-
-
-
 auth_service = Auth()
-
-    
